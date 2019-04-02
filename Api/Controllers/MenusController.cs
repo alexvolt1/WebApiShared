5d45dd9cf2c6f5f11575f9abad86cc0a17b766a3
alexV <alexV@DESKTOP-UKJJB1E>
2019-04-02 00:43:58 -0400

    Add project files.

---
+++ Api/Controllers/MenusController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shared.Data;
+using Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ExpressoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenusController : ControllerBase
+    {
+        WebApiSharedDbContext _webApiSharedDbContext;
+
+        public MenusController(WebApiSharedDbContext webApiSharedDbContext)
+        {
+            _webApiSharedDbContext = webApiSharedDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetMenus()
+        {
+            //var menus = _webApiSharedDbContext.Menus.Include("SubMenus");
+
+            var menus = _webApiSharedDbContext.Menus;
+
+            return Ok(menus);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMenus(int id)
+        {
+            //var menu = _webApiSharedDbContext.Menus.Include("SubMenus").Where(m => m.Id == id);
+            var menu = _webApiSharedDbContext.Menus.Where(m => m.Id == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(menu);
+        }
+
+
+        // POST: api/menus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Image")] Menu menu)
+        {
+            Menu model = new Menu()
+            {
+                Name = "test77",
+                Image = "testUrl77"
+            };
+
+            if (ModelState.IsValid)
+            {
+                _webApiSharedDbContext.Add(model);
+                await _webApiSharedDbContext.SaveChangesAsync();
+                return Ok(menu);
+            }
+            return Ok(menu);
+        }
+
+    }
+}
---
+++ Shared/Data/WebApiSharedDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.Data
+{
+    public class WebApiSharedDbContext : DbContext
+    {
+        public WebApiSharedDbContext(DbContextOptions<WebApiSharedDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Menu> Menus { get; set; }
+
+
+    }
+}
